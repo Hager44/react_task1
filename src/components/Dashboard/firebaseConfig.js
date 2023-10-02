7f4918447769aa60be40698ff9a647874db8c35b
HAGER RAFAAT | Ejada Systems Ltd. <hrafaat@ejadacairo.local>
2023-10-02 16:50:33 +0300

    Your  message

--- src/components/Dashboard/firebaseConfig.js
+++ src/components/Dashboard/firebaseConfig.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyDuFHFYk_YY3ardRbFLG_aRhTUkpmRGEQ4",
-  authDomain: "travel-1313c.firebaseapp.com",
-  projectId: "travel-1313c",
-  storageBucket: "travel-1313c.appspot.com",
-  messagingSenderId: "302022006705",
-  appId: "1:302022006705:web:90f960aef899076be44f90",
-  measurementId: "G-67N9PPJS88"
+  apiKey: "AIzaSyAElfcoRZp7C253Xq7TIk2kPICrdxaYhjE",
+  authDomain: "travelreact-56a55.firebaseapp.com",
+  projectId: "travelreact-56a55",
+  storageBucket: "travelreact-56a55.appspot.com",
+  messagingSenderId: "193133479108",
+  appId: "1:193133479108:web:3eedf93f2f5670d84cb591",
+  measurementId: "G-749MYFRSFR"
 };
 
 // Initialize Firebase
